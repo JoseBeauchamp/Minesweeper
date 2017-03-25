0069ce27fe4f4fba0323ca4dd04c43d9b9ff49b8
Alanis Y. Nazario <alanis.nazario@upr.edu>
2017-03-25 13:51:22 -0400

    .

--- Minesweeper/src/MyMouseAdapterMS.java
+++ Minesweeper/src/MyMouseAdapterMS.java
@@ -121,9 +121,7 @@ public class MyMouseAdapterMS extends MouseAdapter {
 			//Had pressed outside
 			//Do nothing
 			else {
-				if ((gridX == -1) || (gridY == -1)) {
-
-				}
+				if ((gridX == -1) || (gridY == -1)) {}
 				//Is releasing outside
 				//Do nothing
 				else {
@@ -131,113 +129,58 @@ public class MyMouseAdapterMS extends MouseAdapter {
 					//Released the mouse button on a different cell where it was pressed 
 					else {
 						//Released the mouse button on the same cell where it was pressed
-						int neighbor = 0;
-
-						//Top Left
-						if(gridX == 1 && gridY == 1){
-							if(SuperExplosivo.mines[gridX+1][gridY] == MINE){
-								neighbor++;
-							}
-							if(SuperExplosivo.mines[gridX][gridY+ 1]== MINE){
-								neighbor++;
-							}
-							if(SuperExplosivo.mines[gridX+1][gridY+ 1]== MINE){
-								neighbor++;
-							}
-						}
-
-						//bottom left
-						if(gridX == 1 && gridY == 1){
-							if(SuperExplosivo.mines[gridX+1][gridY] == MINE){
-								neighbor++;
-							}
-							if(SuperExplosivo.mines[gridX][gridY - 1]== MINE){
-								neighbor++;
-							}
-							if(SuperExplosivo.mines[gridX + 1][gridY - 1]== MINE){
-								neighbor++;
-							}
-						}
-
-						//Top Right
-						if(gridX == 1 && gridY == 1){
-							if(SuperExplosivo.mines[gridX - 1][gridY] == MINE){
-								neighbor++;
-							}
-							if(SuperExplosivo.mines[gridX][gridY + 1]== MINE){
-								neighbor++;
-							}
-							if(SuperExplosivo.mines[gridX - 1][gridY + 1]== MINE){
-								neighbor++;
-							}
-						}
-
-						//Bottom Right
-						if(gridX == 1 && gridY == 1){
-							if(SuperExplosivo.mines[gridX - 1][gridY] == MINE){
-								neighbor++;
-							}
-							if(SuperExplosivo.mines[gridX][gridY - 1]== MINE){
-								neighbor++;
-							}
-							if(SuperExplosivo.mines[gridX - 1][gridY - 1]== MINE){
-								neighbor++;
-							}
-						}
-
-						//inside of grid nonborder
 						if((gridX != 1 && gridX != 9) && (gridY != 1 && gridY != 9)){
+							int neighbor = 0;
 
-
-							for(int i=0; i < 3 ; i++){
+							for(int i=0; i <3 ; i++){
 								if(SuperExplosivo.mines[gridX -1 + i][gridY - 1]== MINE){
-									neighbor++;
+									neighbor = neighbor + 1;
 								}
 							}
-							for(int i=0; i < 3 ; i++){
+							for(int i=0; i <3 ; i++){
 								if(SuperExplosivo.mines[gridX -1 + i][gridY + 1]== MINE){
-									neighbor++;
+									neighbor = neighbor + 1;
 								}
 							}
 
 							if(SuperExplosivo.mines[gridX -1][gridY]== MINE){
-								neighbor++;
+								neighbor = neighbor + 1;
 							}
 							if(SuperExplosivo.mines[gridX +1][gridY]== MINE){
-								neighbor++;
+								neighbor = neighbor + 1;
 							}
-						}
-						System.out.println(neighbor);
-						// print on tile
-
-						switch (neighbor) {
-						case 0:
-
-							break;
-						case 1:
-
-							break;
-						case 2:
-
-							break;
-						case 3:
-
-							break;
-						case 4:
-							break;
-						case 5:
-							break;
-						case 6:
-
-							break;
-						case 7:
-
-							break;
-						case 8:
-
-							break;
-						}
-
+							System.out.println(neighbor);
+							// print on tile
+							
+								switch (neighbor) {
+								case 0:
+									
+									break;
+								case 1:
+									
+									break;
+								case 2:
+									
+									break;
+								case 3:
+									
+									break;
+								case 4:
+									break;
+								case 5:
+									break;
+								case 6:
+									
+									break;
+								case 7:
+									
+									break;
+								case 8:
+									
+									break;
+								}
+							}
+						
 						for(int i = 1; i < 10; i++){
 							for(int j = 1; j < 10; j++){
 								if(SuperExplosivo.mines[i][j] == MINE){
